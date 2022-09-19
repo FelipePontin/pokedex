8d5bde74d103d8b830d911bd630e7488991085ce
FelipePontin <felipepan64@gmail.com>
2022-09-18 21:31:25 -0300

    pokedex

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Pokedex.css';
+import Logo from '../src/assets/logo.png'
 import axios from 'axios';
 
 const Pokedex = () => {
@@ -44,7 +45,7 @@ const Pokedex = () => {
     <>
       {isSelected && (
         <div className='modal' onClick={() => setIsSelected(false)}>
-          <div className='pokemonSelected'>
+          <div className='pokemonSelected' onClick={(e) => e.stopPropagation()}>
             <img className='imageSelected' src={selectedPokemon.sprites.other.home.front_default}></img>
             <div className='divisionSelected'>
               <h1 className='nameSelected'>{selectedPokemon.name}</h1>
@@ -60,28 +61,36 @@ const Pokedex = () => {
           </div>
         </div>
       )}
-      <section className={!isSelected ? 'pokedex' : 'pokedex pokedexSelected'}>
-        {pokemons.map(pokemon => {
-          const typeInfo = (pokemon.types).map(types => types.type.name)
-          return (
-            <div className='pokemon' onClick={() => getSelectedPokemon(pokemon)}>
-              <img className='image' src={pokemon.sprites.other.home.front_default}></img>
-              <div>
-                <div className='division'>
-                  <h1 className='name'>{pokemon.name}</h1>
-                  <span className='order'>#{pokemon.order}</span>
+      <section className={!isSelected ? 'pokeapi' : 'pokeapi pokeapiSelected'}>
+        <div className='logo' >
+          <img src={Logo}></img>
+        </div>
+        <div class='inputDivision'>
+          <input className='input' type='text' placeholder='Procure por um Pokemon'></input>
+        </div>
+        <div className='pokedex'>
+          {pokemons.map(pokemon => {
+            const typeInfo = (pokemon.types).map(types => types.type.name)
+            return (
+              <div className='pokemon' onClick={() => getSelectedPokemon(pokemon)}>
+                <img className='image' src={pokemon.sprites.other.home.front_default}></img>
+                <div>
+                  <div className='division'>
+                    <h1 className='name'>{pokemon.name}</h1>
+                    <span className='order'>#{pokemon.order}</span>
+                  </div>
+                  <ul className='types'>
+                    {typeInfo.map(type => {
+                      return (
+                        <li className={`type ${type}`}>{type}</li>
+                      )
+                    })}
+                  </ul>
                 </div>
-                <ul className='types'>
-                  {typeInfo.map(type => {
-                    return (
-                      <li className={`type ${type}`}>{type}</li>
-                    )
-                  })}
-                </ul>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
       </section>
     </>
   )
